ae141b68cd609c46f9094926d9866f05beccf0c4
zhonghcc <zhonghcc@gmail.com>
2017-02-09 00:22:53 +0800

    update p0005

--- src/main/java/zhonghcc/P0005_LongestPalindromicSubstring/Solution.java
+++ src/main/java/zhonghcc/P0005_LongestPalindromicSubstring/Solution.java
@@ -2,10 +2,51 @@ package zhonghcc.P0005_LongestPalindromicSubstring;
 
 /**
  * Created by zhong on 2017/2/6.
+ *
+ Given a string s, find the longest palindromic substring in s. You may assume that the maximum length of s is 1000.
+
+ Example:
+
+ Input: "babad"
+
+ Output: "bab"
+
+ Note: "aba" is also a valid answer.
+ Example:
+
+ Input: "cbbd"
+
+ Output: "bb"
  */
 public class Solution {
     public String longestPalindrome(String s) {
 
-        return null;
+        if(s.length()<=0)return "";
+        char[] arr = s.toCharArray();
+        int max = 1;
+        String maxStr = String.valueOf(arr[0]);
+        for(int i=0;i<s.length();i++){
+            for(int j=s.length()-1;j>=i+max;j--){
+                if(i-j==1){break;}
+                if(isPalindrome(arr,i,j)){
+                    int len = j-i+1;
+                    if(len>max){
+                        max=len;
+                        maxStr = s.substring(i,j+1);
+
+                    }
+                }
+            }
+        }
+        return maxStr;
+    }
+    public boolean isPalindrome(char[] arr,int start,int end){
+        for(int i=start;i<=(start+end)/2;i++){
+            int b = end-(i-start);
+            if(arr[i]!=arr[b]){
+                return false;
+            }
+        }
+        return true;
     }
 }
--- src/test/java/zhonghcc/P0005_LongestPalindromicSubstring/SolutionTest.java
+++ src/test/java/zhonghcc/P0005_LongestPalindromicSubstring/SolutionTest.java
@@ -15,5 +15,24 @@ public class SolutionTest {
 
         Solution s = new Solution();
         Assert.assertEquals("bab",s.longestPalindrome("babad"));
+        Assert.assertEquals("bb",s.longestPalindrome("cbbd"));
+        Assert.assertEquals("a",s.longestPalindrome("abcd"));
+        Assert.assertEquals("",s.longestPalindrome(""));
+        Assert.assertEquals("abbaabba",s.longestPalindrome("ababbaabbaab"));
+        Assert.assertEquals("cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc",
+                s.longestPalindrome("cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc"));
+    }
+
+    @Test
+    public void testIsPalindrome(){
+        Solution s = new Solution();
+        String str = "aba";
+        Assert.assertEquals(s.isPalindrome(str.toCharArray(),0,str.length()-1),true);
+        str = "bb";
+        Assert.assertEquals(s.isPalindrome(str.toCharArray(),0,str.length()-1),true);
+        str = "abb";
+        Assert.assertEquals(s.isPalindrome(str.toCharArray(),0,str.length()-1),false);
+        str = "abbaabba";
+        Assert.assertEquals(s.isPalindrome(str.toCharArray(),0,str.length()-1),true);
     }
 }
