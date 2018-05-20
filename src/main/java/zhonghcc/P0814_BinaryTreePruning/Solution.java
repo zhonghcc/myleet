be55c96ab50d59079fa49a6777200227e0280408
zhonghcc <zhonghcc>
2018-05-21 00:25:16 +0800

    accepted p0804

--- src/main/java/zhonghcc/P0814_BinaryTreePruning/Solution.java
+++ src/main/java/zhonghcc/P0814_BinaryTreePruning/Solution.java
@@ -11,7 +11,38 @@ package zhonghcc.P0814_BinaryTreePruning;
  */
 class Solution {
     public TreeNode pruneTree(TreeNode root) {
+        TreeNode newRoot = new TreeNode(root.val);
+        boolean ret = isKeep(root,newRoot);
+        if(ret==true){
+            return newRoot;
+        }else{
+            return null;
+        }
+    }
 
+    boolean isKeep(TreeNode node,TreeNode newNode){
+        boolean ret = false;
+        if(node.left!=null){
+            TreeNode leftNode = new TreeNode(node.left.val);
+            boolean leftKeep = isKeep(node.left,leftNode);
+            if(leftKeep==true){
+                ret = true;
+                newNode.left = leftNode;
+            }
+        }
+        if(node.right!=null){
+            TreeNode rightNode = new TreeNode(node.right.val);
+            boolean rightKeep = isKeep(node.right,rightNode);
+            if(rightKeep==true){
+                ret = true;
+                newNode.right = rightNode;
+            }
+        }
+        if(ret == true || node.val==1){
+            return true;
+        }else{
+            return false;
+        }
     }
 }
 
